05132c9f58067c22139ef982838a782d3ec06702
Alina Tabachnikas <alina.tabachnikas@skyeng.ru>
2026-02-01 21:01:50 +0200

    добавлен гитигноре

--- dist/src/middlewares/errorHandler.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function errorHandler(err, _req, res, _next) {
-    // Basic centralized error handler
-    // Log full error on server
-    // Prefer logging error stack if available
-    // eslint-disable-next-line no-console
-    console.error(err);
-    res.status(500).json({ error: 'Internal Server Error' });
-}
--- dist/src/routes/healthRoutes.js
+++
@@ -1,5 +0,0 @@
-import { Router } from 'express';
-import { health } from '../controllers/healthController.js';
-const router = Router();
-router.get('/health', health);
-export default router;
